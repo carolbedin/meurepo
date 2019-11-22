7806d3dae3d385fbfcff77e4ef33323348dcd01a
Carolina Bedin <carollbedin@gmail.com>
2019-11-21 22:13:03 -0300

    exercicios de js

---
+++ console.js
@@ -0,0 +1,19 @@
+/*
+
+Executar três operações matematicas, solicitadas abaixo:
+
+ subtração, divisão, porcentagem, multiplicação, adição
+
+*/
+
+let idadeMae = 60;
+let idadePai = 70;
+let soma = idadeMae + idadePai // 130
+
+let manga = 10;
+let abacaxi = 5;
+let subtracao = manga - abacaxi // 5
+
+let balas = 50
+let pirulitos = 5
+let divisao = balas / pirulitos //10
